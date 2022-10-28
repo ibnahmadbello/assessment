4fc2df4a3d71d0769fecd5e9522c29ce8155a45e
ibnahmadbello <justforlearningcode@gmail.com>
2022-10-28 05:34:31 +0100

    Book Entity

--- src/main/java/com/arab/assessment/controllers/LibrabrianController.java
+++ src/main/java/com/arab/assessment/controllers/LibrabrianController.java
@@ -1,9 +1,38 @@
 package com.arab.assessment.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.arab.assessment.entities.Book;
+import com.arab.assessment.repository.BookRepository;
 
 @RestController
 public class LibrabrianController {
+	
+	@Autowired
+	BookRepository bookRepository;
+	
+	@PostMapping("/books/uploadBook")
+	public String uploadBook(@RequestBody Book book) {
+		bookRepository.save(book);
+		return book.getTitle() + " saved successfully";
+	}
+	
+	@PutMapping("/books/{id}")
+	public void updateBook(@RequestBody Book book, @PathVariable Integer id) {
+		Book existingBook = bookRepository.findById(id).get();
+		bookRepository.save(book);
+	}
+	
+	@GetMapping("/books/checkedout")
+	public void checkedOutBooks() {}
+	
+	public void readersDetail() {}
 	
 	
 	
--- src/main/java/com/arab/assessment/entities/Book.java
+++ src/main/java/com/arab/assessment/entities/Book.java
@@ -2,17 +2,94 @@ package com.arab.assessment.entities;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "books")
 public class Book {
 	
 	//TODO cover page image
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "book_id")
+	private String id;
+	
+	@Column(nullable = false)
 	private String title;
+	
+	@Column(nullable = false)
 	private String isbn;
-	private String revision_number;
-	private Date published_date;
+	
+	@Column(nullable = false, name = "revision_number")
+	private String revisionNumber;
+	
+	@Column(nullable = false, name = "published_date")
+	private Date publishedDate;
+	
+	
 	private String publisher;
 	private String author;
 	private Date date_added;
 	private String genre;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getRevision_number() {
+		return revision_number;
+	}
+	public void setRevision_number(String revision_number) {
+		this.revision_number = revision_number;
+	}
+	public Date getPublished_date() {
+		return published_date;
+	}
+	public void setPublished_date(Date published_date) {
+		this.published_date = published_date;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public Date getDate_added() {
+		return date_added;
+	}
+	public void setDate_added(Date date_added) {
+		this.date_added = date_added;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
 	
 	//TODO-- author (or authors)
 
